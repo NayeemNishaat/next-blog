36e0b90accc338358aa831439d3888081375a4d9
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-04 01:28:20 +0600

    Implemented Post Method

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,62 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const emailInputRef = useRef();
+	const feedbackInputRef = useRef();
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+	function submitFormHandler(e) {
+		e.preventDefault();
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+		const enteredEmail = emailInputRef.current.value;
+		const enteredFeedback = feedbackInputRef.current.value;
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+		const reqBody = { email: enteredEmail, text: enteredFeedback };
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+		// Important: If we write "/" in the beginning then the domain is automatically appended to the path.
+		fetch("/api/feedback", {
+			method: "POST",
+			body: JSON.stringify(reqBody),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+	return (
+		<>
+			<Head>
+				<title>Next API</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<h1 className="mb-5">The Main Page</h1>
+				<form onSubmit={submitFormHandler}>
+					<div>
+						<label htmlFor="email">Email Address</label>
+						<input
+							className="ring-1 outline-none ml-2"
+							type="email"
+							name="email"
+							id="email"
+							ref={emailInputRef}
+						/>
+					</div>
+					<div>
+						<label htmlFor="feedback">Feedback</label>
+						<textarea
+							className="ml-2 ring-1 mt-3 outline-none"
+							name="feedback"
+							rows="5"
+							id="feedback"
+							ref={feedbackInputRef}
+						></textarea>
+					</div>
+					<button className="ring-1 mt-3">Send Feedback</button>
+				</form>
+			</main>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+			<footer></footer>
+		</>
+	);
 }
