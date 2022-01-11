96b42a0f9d39430e47274313bdb89f7b7a1fe659
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-12 04:04:35 +0600

    Optimized

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function HomePage() {
 	return (
 		<>
-			<h1 className="text-6xl text-pink-700">Home Page</h1>
+			<h1 className="text-9xl text-center mt-20 text-pink-700">
+				Home Page
+			</h1>
 		</>
 	);
 }
