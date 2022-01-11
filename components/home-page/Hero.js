58b62b32262e15ca030a52d8e292b702426c5acd
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-12 04:43:39 +0600

    Working on Hero

---
+++ components/home-page/Hero.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+function Hero() {
+	return (
+		<section className="text-center bg-gradient-to-b from-gray-900 to-gray-800 p-8">
+			<div
+				className="max-w-[18rem]
+			 shadow-lg rounded-full overflow-hidden bg-gray-700 m-auto"
+			>
+				<Image
+					src="/images/site/nayeem.jpg"
+					alt="An image showing Nayeem"
+					objectFit="cover"
+					width={300}
+					height={300}
+				/>
+			</div>
+			<h1 className="mt-4 text-gray-300 text-7xl">Hi! I'm Nayeem!</h1>
+			<p className="text-gray-200 mx-auto w-4/5">
+				Welcome to my blog! I'm a js/nodeJs developer.
+			</p>
+		</section>
+	);
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import Hero from "../components/home-page/hero";
+
 function HomePage() {
 	return (
 		<>
-			<h1 className="text-9xl text-center mt-20 text-pink-700">
-				Home Page
-			</h1>
+			<Hero />
 		</>
 	);
 }
