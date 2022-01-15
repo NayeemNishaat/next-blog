1591072ad487cc02ce8e9be2af6a3c347a712381
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-16 00:52:30 +0600

    Working with md

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,21 +1,39 @@
 import PostHeader from "./PostHeader";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
-const DUMMY_POST = {
-	slug: "getting-started",
-	title: "Getting Started",
-	image: "getting-started.png",
-	date: "2022-02-10",
-	content: "# This is a first post"
-};
+function PostContent(props) {
+	const { post } = props;
 
-function PostContent() {
-	const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+	const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+	const customComponent = {
+		p: ({ children, node }) => {
+			const imageEl = node.children[0];
+
+			if (imageEl.tagName === "img") {
+				return (
+					<div className="mx-auto my-4 w-full max-w-xl">
+						<Image
+							{...props}
+							src={`/images/posts/${post.slug}/${imageEl.properties.src}`}
+							alt={imageEl.properties.alt}
+							width={600}
+							height={300}
+						/>
+					</div>
+				);
+			}
+			return <p>{children}</p>;
+		}
+	};
 
 	return (
 		<article className="w-[95%] max-w-5xl my-2 mx-auto text-2xl bg-gray-100 rounded-md p-4 md:p-8">
-			<PostHeader title={DUMMY_POST.title} image={imagePath} />
-			<ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+			<PostHeader title={post.title} image={imagePath} />
+			<ReactMarkdown components={customComponent}>
+				{post.content}
+			</ReactMarkdown>
 		</article>
 	);
 }
